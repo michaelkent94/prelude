b2dd4c04639b328d68c8dd2e491f3da65b20a47f
Michael Kent <michaelkent94@me.com>
2017-10-06 06:35:58 -0500

    Add personal.el

---
+++ personal/personal.el
@@ -0,0 +1,21 @@
+;; Company and Company SourceKit
+(defun company-sourcekit-init ()
+  "Enable sourcekit backend for company."
+  (eval-after-load 'company
+    '(add-to-list
+      (make-local-variable 'company-backends)
+      'company-sourcekit)))
+
+(add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'swift-mode-hook 'company-sourcekit-init)
+
+(global-set-key (kbd "<backtab>") 'company-complete-common)
+
+;; Flycheck Swift
+(eval-after-load 'flycheck '(flycheck-swift-setup))
+
+;; Yasnippet
+(add-hook 'prog-mode-hook 'yas-minor-mode)
+
+;; Add a nice binding to open project explorer
+(global-set-key (kbd "C-c C-p") 'project-explorer-open)
